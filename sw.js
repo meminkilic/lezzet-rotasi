41e87db7a619687fc91bd4bfedda9dfeadeb22ff
Mehmet Emin KILIÇ <65869143+meminkilic@users.noreply.github.com>
2026-07-04 17:07:17 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// Rotaste — Service Worker V1.10.5
-const CACHE = 'lezzet-v1.10.5';
+// Rotaste — Service Worker V1.11.0
+const CACHE = 'lezzet-v1.11.0';
 const STATIC = ['/', '/manifest.json'];
 
 self.addEventListener('install', e => {
